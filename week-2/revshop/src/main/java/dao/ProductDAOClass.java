47d66037367ac5f4425fa636b47293b7e8679478
krishnagopika <103101208+krishnagopika@users.noreply.github.com>
2024-08-02 16:01:55 +0530

    Update ProductDAOClass.java

--- week-2/revshop/src/main/java/dao/ProductDAOClass.java
+++ week-2/revshop/src/main/java/dao/ProductDAOClass.java
@@ -1,127 +1,52 @@
-package dao;
+package utils;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
 
-import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.jdbc.Driver;
 
-import dto.ProductRequest;
-import dto.ProductResponse;
-import utils.ConnectionFactory;
-
-public class ProductDAOClass implements ProductDao{
+public class ConnectionFactory {
 	
-
-	@Override
-	public ProductResponse getProductById(long id) {
-		String sql =  "SELECT * FROM Products WHERE ID = ?";
-		
-		try(Connection con = ConnectionFactory.getConnectionFactory().getConnection(); PreparedStatement stmt = con.prepareStatement(sql)){
-			
-			stmt.setFloat(1, id);
-			
-			try(ResultSet rs = stmt.executeQuery()){
-				
-				if(rs.next()) {
-					return new ProductResponse(
-							rs.getLong("id"),
-							rs.getString("name"),
-							rs.getString("sku_code"),
-							rs.getFloat("price")
-							);
-				}
-				
-			}
+	private static final ConnectionFactory connectionFactory = new ConnectionFactory();
+	
+	private Properties props = new Properties();
+	
+	private ConnectionFactory() {
+		try {
+			props.load(new FileReader("src/main/resources/db.properties"));
 			
 		}
-		catch (SQLException e) {
+		catch(IOException e ) {
+			e.printStackTrace();		
+		}	
+	}
+	
+	public static ConnectionFactory getConnectionFactory() {
+		return connectionFactory;
+	}
+	
+	public Connection getConnection() {
+		
+		try {
+			//Class.forName("com.mysql.jdbc.Driver") can be ignored
+			return DriverManager.getConnection(
+					props.getProperty("url"),
+					props.getProperty("username"),
+					props.getProperty("password")
+					);
+		} catch (SQLException e) {
 			e.printStackTrace();
-			
 		}
-		
 		
 		return null;
+		
 	}
-
-	@Override
-	public boolean createProduct(ProductRequest productRequest) {
-		String sql =  "INSERT INTO Products(name, sku_code, price) VALUES(?,?,?)";
-		
-		try(Connection con = ConnectionFactory.getConnectionFactory().getConnection(); PreparedStatement stmt = con.prepareStatement(sql)){
-			
-			stmt.setString(1, productRequest.getName());
-			stmt.setString(2, productRequest.getSkuCode());
-			stmt.setFloat(3, productRequest.getPrice());
-			
-			int result = stmt.executeUpdate();
-			
-			if(result>0) {
-				
-				return true;	
-			}
-			
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			
-		}
-		
-		
-		return false;
-	}
-
-	@Override
-	public boolean updateProduct(ProductResponse productResponse) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean deleteProduct(long id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<ProductResponse> getAllProducts() {
-		String sql =  "SELECT * FROM Products";
-		
-		List<ProductResponse> products = new ArrayList();
-		
-		try(Connection con = ConnectionFactory.getConnectionFactory().getConnection(); PreparedStatement stmt = con.prepareStatement(sql)){
-						
-			try(ResultSet rs = stmt.executeQuery()){
-				
-				ProductResponse response = null;
-				
-				while(rs.next()) {
-					
-					response = new ProductResponse(
-								rs.getLong("id"),
-								rs.getString("name"),
-								rs.getString("sku_code"),
-								rs.getFloat("price")
-								);
-					
-					products.add(response);
-				}
-				
-				return products;
-				
-			}
-			
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			
-		}
-		
-		
-		return null;
-	}
+	
+	
+	
 
 }
